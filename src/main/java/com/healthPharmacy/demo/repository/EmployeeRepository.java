667d28f5c03e8ef41eaaffb1b9762a046c54422e
Paulo <pauloaraujobarbosa1427@gmail.com>
2025-06-24 20:41:56 -0300

    feat/ add function remove employee

--- src/main/java/com/healthPharmacy/demo/repository/EmployeeRepository.java
+++ src/main/java/com/healthPharmacy/demo/repository/EmployeeRepository.java
@@ -2,9 +2,11 @@ package com.healthPharmacy.demo.repository;
 
 import com.healthPharmacy.demo.models.EmployeeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeModel, Long> {
+    Optional<EmployeeModel> findByPersonModelId(Long id);
 }
--- src/main/java/com/healthPharmacy/demo/services/EmployeeService.java
+++ src/main/java/com/healthPharmacy/demo/services/EmployeeService.java
@@ -5,6 +5,7 @@ import com.healthPharmacy.demo.models.EmployeeModel;
 import com.healthPharmacy.demo.enums.UserRole;
 import com.healthPharmacy.demo.models.PersonModel;
 import com.healthPharmacy.demo.repository.EmployeeRepository;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -27,4 +28,12 @@ public class EmployeeService {
         employeeRepository.save(employeeModel);
     }
 
+    public void deleteEmployee(Long id) {
+        EmployeeModel employeeModel = employeeRepository.findByPersonModelId(id).orElseThrow(() -> new UsernameNotFoundException("Employee not found"));
+        if (!employeeModel.getPersonModel().isActive()) throw new UsernameNotFoundException("Employee not found");
+
+        PersonModel personModel = employeeModel.getPersonModel();
+        personModel.setActive(false);
+        employeeRepository.save(employeeModel);
+    }
 }
